517b49d3456366e2765313cf256c759525f957a3
abdelwahedyosri <52857196+abdelwahedyosri@users.noreply.github.com>
2024-05-31 23:26:44 +0100

    Add files via upload
    first commit

---
+++ src/main/java/group_6/e_contractor_backend/EContractorBackendApplication.java
@@ -0,0 +1,13 @@
+package group_6.e_contractor_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EContractorBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EContractorBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/group_6/e_contractor_backend/EContractorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package group_6.e_contractor_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EContractorBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
