88df1656e9f9e869eef99b89b125aba23d0eae4c
rihenhouli <rihenhoulii@gmail.com>
2024-06-01 12:57:08 +0100

    fix/EC-5_job_offer_module_repositories

---
+++ src/main/java/group_6/e_contractor_backend/job_offer_module/repositories/JobApplicationAppointmentRepository.java
@@ -0,0 +1,7 @@
+package group_6.e_contractor_backend.job_offer_module.repositories;
+
+import group_6.e_contractor_backend.job_offer_module.entities.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobApplicationAppointmentRepository extends JpaRepository<JobApplicationAppointment, Long>{
+}
---
+++ src/main/java/group_6/e_contractor_backend/job_offer_module/repositories/JobApplicationFileRepository.java
@@ -0,0 +1,6 @@
+package group_6.e_contractor_backend.job_offer_module.repositories;
+import group_6.e_contractor_backend.job_offer_module.entities.JobApplicationFile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobApplicationFileRepository extends JpaRepository<JobApplicationFile, Long>{
+}
